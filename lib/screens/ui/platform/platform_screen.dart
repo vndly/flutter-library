3d77606d065843d88562b41b787b13766e159c00
Mauricio Togneri <mauricio.togneri@gmail.com>
2019-07-07 15:53:25 +0200

    Improved platform example

--- lib/screens/ui/platform/platform_screen.dart
+++ lib/screens/ui/platform/platform_screen.dart
@@ -1,4 +1,5 @@
-import 'dart:io';
+import 'package:dafluta/dafluta.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class PlatformScreen extends StatefulWidget {
@@ -8,17 +9,33 @@ class PlatformScreen extends StatefulWidget {
 
 class _PlatformScreenState extends State<PlatformScreen> {
   bool switchSelected = true;
+  double sliderValue = 50;
+  bool asAndroid = true;
+
+  void _changeOS(bool android) {
+    setState(() {
+      asAndroid = android;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Platform: ${Platform.operatingSystem}'),
+        title: const Text('Platform'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.refresh),
+            onPressed: () => _changeOS(!asAndroid),
+          ),
+        ],
       ),
       body: Center(
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
+            Text('Platform: ${asAndroid ? 'Android' : 'iOS'}'),
+            const VBox(20),
             Switch.adaptive(
               value: switchSelected,
               onChanged: (value) {
@@ -27,6 +44,43 @@ class _PlatformScreenState extends State<PlatformScreen> {
                 });
               },
             ),
+            const VBox(20),
+            asAndroid
+                ? const CircularProgressIndicator()
+                : CupertinoActivityIndicator(),
+            const VBox(20),
+            asAndroid
+                ? RaisedButton(
+                    onPressed: () {},
+                    child: const Text('Android button'),
+                  )
+                : CupertinoButton(
+                    onPressed: () {},
+                    color: Colors.blue,
+                    child: const Text('iOS button'),
+                  ),
+            const VBox(20),
+            asAndroid
+                ? Slider(
+                    onChanged: (value) {
+                      setState(() {
+                        sliderValue = value;
+                      });
+                    },
+                    value: sliderValue,
+                    min: 0,
+                    max: 100,
+                  )
+                : CupertinoSlider(
+                    onChanged: (value) {
+                      setState(() {
+                        sliderValue = value;
+                      });
+                    },
+                    value: sliderValue,
+                    min: 0,
+                    max: 100,
+                  ),
           ],
         ),
       ),
