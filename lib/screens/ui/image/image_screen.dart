5d2d97ba71c101c1fd10f2d18a0e3a1dd49b67f4
Mauricio Togneri <mauricio.togneri@gmail.com>
2019-07-15 23:53:43 +0200

    Improved image loading

--- lib/screens/ui/image/image_screen.dart
+++ lib/screens/ui/image/image_screen.dart
@@ -1,25 +1,68 @@
 import 'dart:math';
 
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 class ImageScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final index = Random().nextInt(100);
-
     return Scaffold(
       appBar: AppBar(
         title: const Text('Image'),
       ),
       body: Center(
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(15),
-          child: FadeInImage.assetNetwork(
-            placeholder: 'assets/gifs/loading.gif',
-            image: 'https://picsum.photos/250?image=$index',
+        child: image2(),
+      ),
+    );
+  }
+
+  Widget image1() {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(15),
+      child: FadeInImage.assetNetwork(
+        placeholder: 'assets/gifs/loading.gif',
+        image: url(),
+      ),
+    );
+  }
+
+  Widget image2() {
+    return CachedNetworkImage(
+      imageUrl: url(),
+      imageBuilder: (context, imageProvider) => SizedBox(
+        width: 200,
+        height: 200,
+        child: Image(
+          image: imageProvider,
+          fit: BoxFit.cover,
+        ),
+      ),
+      placeholder: (context, url) => SizedBox(
+        width: 200,
+        height: 200,
+        child: Container(
+          color: Colors.grey[300],
+          child: Center(
+            child: const CircularProgressIndicator(),
+          ),
+        ),
+      ),
+      errorWidget: (context, url, error) => SizedBox(
+        width: 200,
+        height: 200,
+        child: Container(
+          color: Colors.grey[300],
+          child: Center(
+            child: Icon(Icons.error),
           ),
         ),
       ),
     );
+  }
+
+  String url() {
+    final index = Random().nextInt(100);
+
+    return 'https://picsum.photos/250?image=$index';
   }
 }
